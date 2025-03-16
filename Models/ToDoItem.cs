891378d8155b829e91d83ca63052ea0d91d25b27
NickSlm <solomonovnick@gmail.com>
2025-03-16 10:43:31 +0200

    Refactoring directory structure

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+public class ToDoItem: INotifyPropertyChanged
+{
+
+
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+	public ICommand ToggleReadOnlyCommand => _toggleReadOnlyCommand;
+	private DelegateCommand _toggleReadOnlyCommand;
+
+	private bool _isReadOnly;
+	public bool IsReadOnly
+	{
+		get => _isReadOnly;
+		set
+		{
+			_isReadOnly = value;
+			OnPropertyChanged(nameof(IsReadOnly));
+		}
+	}
+
+	private bool _isComplete;
+
+	public bool IsComplete
+	{
+		get => _isComplete;
+		set
+		{
+			_isComplete = value;
+			OnPropertyChanged(nameof(IsComplete));
+		}
+	}
+
+	private string? _title;
+	public string Title {
+		get => _title;
+		set
+		{
+			_title = value;
+			OnPropertyChanged(nameof(Title));
+		}
+	}
+
+	public ToDoItem(string title)
+	{
+		Title = title;
+        _isReadOnly = true;
+        _isComplete = false;
+        _toggleReadOnlyCommand = new DelegateCommand(ToggleReadOnly, canToggle);
+
+    }
+
+	private void ToggleReadOnly(object commandParameter)
+	{
+		IsReadOnly = !IsReadOnly;
+	}
+
+	private bool canToggle(object commmandParameter)
+	{
+		return true;
+	}
+
+    public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+}
---
+++ ViewModels/ToDoListViewModel.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+public class ToDoListViewModel: INotifyPropertyChanged
+{
+    public ICommand AddItemCommand => _addItemCommand;
+    public ICommand RemoveItemCommand => _removeItemCommand;
+    public ICommand CleanItemsCommand => _cleanItemsCommand;
+
+
+    private readonly DelegateCommand _addItemCommand;
+    private readonly DelegateCommand _removeItemCommand;
+    private readonly DelegateCommand _cleanItemsCommand;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public ObservableCollection<ToDoItem> ToDoList { get; set; }
+
+    private string _itemText;
+    public string ItemText
+    {
+        get => _itemText;
+        set
+        { 
+            _itemText = value;
+            OnPropertyChanged(nameof(ItemText));
+            _addItemCommand.InvokeCanExecuteChanged();
+        }
+    }
+
+    private int _totalTasks;
+    public int TotalTasks
+    {
+        get => _totalTasks;
+        set
+        {
+            _totalTasks = value;
+            OnPropertyChanged(nameof(TotalTasks));
+        }
+    }
+
+    private int _completedTasks;
+    public int CompletedTasks
+    {
+        get => _completedTasks;
+        set
+        {
+            _completedTasks = value;
+            OnPropertyChanged(nameof(CompletedTasks));
+        }
+    }
+
+    public ToDoListViewModel()
+	{
+        ToDoList = new ObservableCollection<ToDoItem>();
+
+        ToDoList.CollectionChanged += (s, e) => UpdateTotalTasks();
+        ToDoList.CollectionChanged += (s, e) => HandleCollectionChanged(e);
+
+        _addItemCommand = new DelegateCommand(AddItem, CanAddItem);
+        _removeItemCommand = new DelegateCommand(RemoveItem, CanRemoveItem);
+        _cleanItemsCommand = new DelegateCommand(CleanCompletedItems, CanCleanCompletedItems);
+
+        UpdateCompletedTasks();
+    }
+
+    private void UpdateTotalTasks()
+    {
+        TotalTasks = ToDoList.Count();
+    }
+
+    private void UpdateCompletedTasks()
+    {
+        CompletedTasks = (TotalTasks > 0) ? (ToDoList.Count(item => item.IsComplete) * 100) / TotalTasks : 0;
+    }
+
+    private void Item_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(ToDoItem.IsComplete))
+        {
+            UpdateCompletedTasks();
+        }
+    }
+
+    private void HandleCollectionChanged(NotifyCollectionChangedEventArgs e)
+    {
+        if (e.Action == NotifyCollectionChangedAction.Add)
+        {
+            foreach (ToDoItem newItem in e.NewItems)
+            {
+                newItem.PropertyChanged += Item_PropertyChanged;
+            }
+        }
+        if (e.Action == NotifyCollectionChangedAction.Remove)
+        {
+            foreach (ToDoItem newItem in e.OldItems)
+            {
+                newItem.PropertyChanged -= Item_PropertyChanged;
+            }
+        }
+        UpdateCompletedTasks();
+    }
+
+    private void AddItem(object commandParameter)
+    {
+        ToDoList.Add(new ToDoItem(ItemText));
+        ItemText = "";
+        _addItemCommand.InvokeCanExecuteChanged();
+    }
+
+    private bool CanAddItem(object commandParameter)
+    {
+        return !string.IsNullOrWhiteSpace(ItemText);
+    }
+
+    private void RemoveItem(object commandParameter)
+    {
+        ToDoList.Remove((ToDoItem)commandParameter);
+        _addItemCommand.InvokeCanExecuteChanged();
+    }
+
+    private bool CanRemoveItem(object commandParameter)
+    {
+        return commandParameter is ToDoItem item && ToDoList.Contains(item);
+    }
+
+    private void CleanCompletedItems(object commandParameter)
+    {
+        if (commandParameter is ObservableCollection<ToDoItem> ToDoList)
+        {
+            for (int i = ToDoList.Count() - 1; i >= 0; i--)
+            {
+                if (ToDoList[i].IsComplete)
+                {
+                    ToDoList.RemoveAt(i);
+                }
+            }
+        }
+    }
+
+    private bool CanCleanCompletedItems(object commandParameter)
+    {
+        return true;
+    }
+
+    public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+}
