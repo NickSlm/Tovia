61503e22ba76336da37b971feb115f09265f1a6d
NickSlm <solomonovnick@gmail.com>
2025-04-03 16:05:31 +0300

    Added Material Design for cleaner gui, Fixed the way popdialog was handled

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,7 +4,6 @@ using System.Windows;
 using ToDoListPlus.Services;
 using Microsoft.Extensions.DependencyInjection;
 using ToDoListPlus.ViewModels;
-using ToDoListPlus.Views;
 using Microsoft.EntityFrameworkCore;
 
 namespace ToDoListPlus;
@@ -38,15 +37,12 @@ public partial class App : Application
         services.AddDbContext<ToDoContext>(options => options.UseSqlite("Data Source=ToDoList.db"));
 
         services.AddSingleton<IDatabaseInitializer, DatabaseInitializer>();
-        services.AddSingleton<IDialogService, DialogService>();
         services.AddSingleton<AuthService>();
 
         services.AddSingleton<ToDoListViewModel>();
         services.AddSingleton<AuthorizationViewModel>();
-        services.AddSingleton<PopupDialogViewModel>();
         services.AddSingleton<MainViewModel>();
 
-        services.AddSingleton<PopupDialogView>();
         services.AddSingleton<MainWindow>();
 
     }
