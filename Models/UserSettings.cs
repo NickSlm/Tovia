2fe70affa9d6a1b03fb1d7d2dbc3821abc70dc24
NickSlm <solomonovnick@gmail.com>
2025-06-19 17:01:47 +0300

    Added new theme service

--- Models/UserSettings.cs
+++ Models/UserSettings.cs
@@ -8,9 +8,8 @@ namespace ToDoListPlus.Models
 {
     public class UserSettings
     {
-
         public Dictionary<string, HotkeySettings> Hotkeys { get; set; } = new();
         public WindowSettings Window { get; set; }
-
+        public ThemeSettings Theme { get; set; }
     }
 }
