b9035fa1b6fe9f366af5b20879a5a2ff36d4a67c
NickSlm <solomonovnick@gmail.com>
2025-07-29 14:10:35 +0300

    update2

--- States/TaskManager.cs
+++ States/TaskManager.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using ToDoListPlus.Models;
 using ToDoListPlus.Services;
 
--- obj/Debug/net8.0-windows/Views/AuthorizationWindow.g.i.cs
+++ obj/Debug/net8.0-windows/Views/AuthorizationWindow.g.i.cs
@@ -56,7 +56,7 @@ namespace ToDoListPlus.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ToDoListPlus;component/views/authorizationwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ToDoListPlus;V1.0.0.0;component/views/authorizationwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\AuthorizationWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
