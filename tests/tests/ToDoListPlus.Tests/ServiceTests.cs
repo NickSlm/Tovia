11762b80c7489547ce846983e73a39489ac97f91
NickSlm <solomonovnick@gmail.com>
2025-07-29 14:39:47 +0300

    Optimized project structure, added Test proj

---
+++ tests/tests/ToDoListPlus.Tests/ServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using ToDoListPlus.ViewModels;
+using Xunit;
+
+namespace ToDoListPlus.Tests
+{
+    public class ServiceTests
+    {
+
+        [Fact]
+        public void test()
+        {
+
+        }
+    }
+}
