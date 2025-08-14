2e32b1bc13b82583d571e28eae64ce9406d8b8db
NickSlm <solomonovnick@gmail.com>
2025-08-14 15:00:52 +0300

    Bitmap Converter

---
+++ src/ToDoListPlus/Converters/ConvertBytesToBitmapImage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ToDoListPlus.Converters
+{
+    public class ConvertBytesToBitmapImage
+    {
+
+
+        public static BitmapImage ConvertToImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0)
+            {
+                return null;
+            }
+
+            using var ms = new MemoryStream(imageData);
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.StreamSource = ms;
+            image.EndInit();
+            image.Freeze();
+            return image;
+        }
+
+    }
+}
