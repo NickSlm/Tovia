efd5a7a587b21bdff59c1262ac70153dec323935
NickSlm <solomonovnick@gmail.com>
2025-11-11 13:21:13 +0200

    DbContext optimization

--- src/Tovia/Data/AppDbContext.cs
+++ src/Tovia/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,25 +12,12 @@ namespace Tovia.Data
 {
     public class AppDbContext: DbContext
     {
-        private readonly string _databasePath;
-        private readonly string _localFolder;
-        private readonly string _appFolder;
 
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
         public DbSet<ToDoItem> ToDoItems { get; set; }
-
-        public AppDbContext()
-        {
-            _localFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            _appFolder = Path.Combine(_localFolder, "Tovia");
-            _databasePath = Path.Combine(_appFolder, "Tovia.db");
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            if (!options.IsConfigured)
-                options.UseSqlite($"Data Source={_databasePath}");
-        }
-
-
     }
 }
+        
